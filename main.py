88940398ddc8ec5b6d858d45d0a5ef76b773dbf2
Neemah Delfanian <nedel21@hotmail.com>
2026-02-07 09:03:35 -0500

    Player ship working

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import pygame 
+import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -10,13 +11,25 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    clk = pygame.time.Clock()
+    dt = 0
+
     while(True):
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updatable.update(dt)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
+        dt = clk.tick(60) / 1000
 
 
 if __name__ == "__main__":
